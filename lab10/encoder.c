a94bb8b60770a60d6f7679173ebcd6b293f88325
Matt Baseheart <basehear@usc.edu>
2021-05-17 11:31:50 -0700

    Update encoder.c

--- lab10/encoder.c
+++ lab10/encoder.c
@@ -3,15 +3,20 @@
 #include <stdio.h>
 #include <avr/interrupt.h>
 
-#include "encoder.h"               			// Declarations of the functions
+// Declarations of the functions
+#include "encoder.h"
 #include "speedTrap.h"
 void encoder_init(void){
-		PORTC |= (1 << PC1) | (1 << PC5);	//Enable pullup resistors, connect PC1 and PC5 to encoder
-		PCICR |= (1<<PCIE1);				//Set interrupt
+		//Enable pullup resistors, connect PC1 and PC5 to encoder
+		PORTC |= (1 << PC1) | (1 << PC5);
+		
+		//Set interrupt
+		PCICR |= (1<<PCIE1);	
 		PCMSK1 |= (1<<PCINT9);
 		PCMSK1 |= (1<<PCINT13);
 		
-		rotInput = PINC;					//get first state
+		//GET FIRST STATE
+		rotInput = PINC;
 			if(rotInput & (1<<PC1))
 				rotA = 1;
 			else
